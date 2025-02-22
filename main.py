8959adab7adcc674c7fa3c324d39e3ea119e3180
piotrek <piotrek@ubuntu.com>
2025-02-22 23:43:15 +0100

    add watermarking to photo

--- main.py
+++ main.py
@@ -5,33 +5,30 @@
 # MacOS
 # brew install python-tk
 
-from tkinter import  *
+from tkinter import *
 from PIL import Image, ImageTk
-
 
 window = Tk()
 window.title('Watermarking app')
-window.config(padx=50,pady=50)
 
-image = Image.open('photo.png')
-img_tk=ImageTk.PhotoImage(image)
+photo = Image.open('JPEG_example_JPG_RIP_050.png').convert('RGBA')
+img_tk = ImageTk.PhotoImage(photo)
 
-watermark=Image.open('watermark.png')
-watermark_resized = watermark.resize((int(0.2*image.width),int(0.2*image.height)))
-watermark_resized_tk=ImageTk.PhotoImage(watermark_resized)
+watermark = Image.open('watermark.png').convert('RGBA')
+watermark_resized = watermark.resize((int(0.2 * photo.width), int(0.2 * photo.height)))
+watermark_resized_tk = ImageTk.PhotoImage(watermark_resized)
 
-
-
-print(image.width)
-print(image.height)
-canvas= Canvas(width=image.width,height=image.height)
+#to zapisuje tylko do 1x1 pixel kurwicy dostane
+# juz nie zapisuje :3 3h w plecy
+canvas = Canvas(width=photo.width, height=photo.height)
 canvas.config(highlightthickness=0)
 
-canvas.create_image(image.width/2,image.height/2,image=img_tk)
-canvas.create_image(image.width*0.9,image.height*0.9,image=watermark_resized_tk)
+canvas.create_image(photo.width / 2, photo.height / 2, image=img_tk)
+canvas.create_image(photo.width * 0.9, photo.height * 0.9, image=watermark_resized_tk)
 canvas.pack()
-canvas.postscript(file='')
+#zawsze canvas update
+canvas.update()
+canvas.postscript(file="canvas_output.ps", colormode='color')
+ps_image = Image.open("canvas_output.ps")
+ps_image.save('wychodek.png')
 
-
-
-window.mainloop()
